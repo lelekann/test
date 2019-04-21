6a87b430ddd6dcf9501cbf786bd4b2b8137e82fb
Ann Leleko <vredina.ann@gmail.com>
2019-04-21 21:02:34 +0300

    test commit

--- wp-content/themes/final/template_parts/homepage/section_5.php
+++ wp-content/themes/final/template_parts/homepage/section_5.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dobby
- * Date: 21.04.19
- * Time: 20:34
- */
+<?php $main_title = get_sub_field('main_title'); ?>
+
+<section class="home_section_5">
+
+  <?php if (!empty($main_title)): ?>
+  <div class="dark-h1">
+    <?php print $main_title; ?>
+  </div>
+<?php endif; ?>
+</section>
