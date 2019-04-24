409480e6fcfe4c5c992283ba14d512066be75d36
Ann Leleko <vredina.ann@gmail.com>
2019-04-24 13:21:51 +0300

    done all without section 5

--- wp-content/themes/final/template_parts/homepage/section_2.php
+++ wp-content/themes/final/template_parts/homepage/section_2.php
@@ -1,24 +1,22 @@
-<?php $text=get_sub_field('description'); ?>
-<?php $link=get_sub_field('link'); ?>
-<?php $video=get_sub_field('video'); ?>
+<?php $text = get_sub_field('description'); ?>
+<?php $link = get_sub_field('link'); ?>
+<?php $video = get_sub_field('video'); ?>
 
 <section class="home_section_2">
 
-    <div class="content_gorizontal">
-      <?php if (!empty($text)): ?>
+  <div class="content_gorizontal">
+    <?php if (!empty($text)): ?>
       <div class="top-description">
         <?php print $text; ?>
       </div>
-      <?php endif; ?>
+    <?php endif; ?>
 
-      <div>
-        <?php if (!empty($video)): ?>
-        <a href="<?php print $video['url']; ?>" class="btn"><?php if (!empty($link)): ?>
-          <?php print $link['title']; ?>
-          <?php endif; ?>
-        </a>
+    <div>
+      <?php if (!empty($link)): ?>
+      <a href="<?php echo $video['url']; ?>" class="btn"> <?php echo $link['title']; ?>
         <?php endif; ?>
-      </div>
+      </a>
     </div>
+  </div>
 
 </section>
--- wp-content/themes/final/template_parts/homepage/section_8.php
+++ wp-content/themes/final/template_parts/homepage/section_8.php
@@ -4,26 +4,26 @@
 
 <section class="home_section_8">
 
-    <div class="content_gorizontal">
-      <div class="content-vertical">
-        <?php if (!empty($title)): ?>
-          <div class="white-h1">
-            <?php print $title; ?>
-          </div>
-        <?php endif; ?>
-
-        <?php if (!empty($text)): ?>
-          <div class="white-description">
-            <?php print $text; ?>
-          </div>
-        <?php endif; ?>
-      </div>
-
-      <?php if( !empty($image) ): ?>
-        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+  <div class="content_gorizontal">
+    <div class="content_vertical">
+      <?php if (!empty($title)): ?>
+        <div class="white-h1">
+          <?php print $title; ?>
+        </div>
       <?php endif; ?>
 
+      <?php if (!empty($text)): ?>
+        <div class="white-description">
+          <?php print $text; ?>
+        </div>
+      <?php endif; ?>
     </div>
+
+    <?php if (!empty($image)): ?>
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>"/>
+    <?php endif; ?>
+
+  </div>
 
 </section>
 
